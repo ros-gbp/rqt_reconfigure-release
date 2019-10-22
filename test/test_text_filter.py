e8e008800e0ebcbf3d0109ff71269e0e64800a85
Scott K Logan <logans@cottsay.net>
2019-10-22 15:22:31 -0700

    Rebase from 'release/kinetic/rqt_reconfigure'

--- test/test_text_filter.py
+++ test/test_text_filter.py
@@ -1,21 +1,21 @@
-# Software License Agreement (BSD License)
-#
 # Copyright (c) 2012, Willow Garage, Inc.
 # All rights reserved.
+#
+# Software License Agreement (BSD License 2.0)
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions
 # are met:
 #
-# * Redistributions of source code must retain the above copyright
-# notice, this list of conditions and the following disclaimer.
-# * Redistributions in binary form must reproduce the above
-# copyright notice, this list of conditions and the following
-# disclaimer in the documentation and/or other materials provided
-# with the distribution.
-# * Neither the name of Willow Garage, Inc. nor the names of its
-# contributors may be used to endorse or promote products derived
-# from this software without specific prior written permission.
+#  * Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+#  * Redistributions in binary form must reproduce the above
+#    copyright notice, this list of conditions and the following
+#    disclaimer in the documentation and/or other materials provided
+#    with the distribution.
+#  * Neither the name of Willow Garage, Inc. nor the names of its
+#    contributors may be used to endorse or promote products derived
+#    from this software without specific prior written permission.
 #
 # THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
 # "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
@@ -34,7 +34,8 @@
 
 import unittest
 
-from python_qt_binding.QtCore import Qt, QRegExp
+from python_qt_binding.QtCore import QRegExp, Qt
+
 from rqt_reconfigure.text_filter import TextFilter
 
 
@@ -62,6 +63,3 @@ class MyTest(unittest.TestCase):
         self.assertEqual(result_regex,
                          True  # Both _query_text & filtered_text overlaps.
                          )
-
-if __name__ == '__main__':
-    unittest.main()
